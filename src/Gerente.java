649ab9b28b8d069f356bb7f5ff7b817589c3916a
Luan Henrique <luan.henriqueepp@gmail.com>
2020-05-10 16:38:11 -0300

    Conceitos de composção e interface

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,20 +1,21 @@
 public class Gerente extends Funcionario implements Autenticavel {
 
-    private int senha;
+    private AutenticacaoUtil autenticador;
+
+    public Gerente() {
+        this.autenticador = new AutenticacaoUtil();
+    }
     public double getBonificacao() {
         return super.getSalario();
     }
 
     @Override
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha) {
-        if (this.senha == senha) {
-            return true;
-        }
-        return false;
+        return this.autenticador.autentica(senha);
     }
 }
